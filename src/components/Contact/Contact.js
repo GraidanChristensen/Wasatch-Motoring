ea5527c4dcec2ae5112279adcd829561ec04de73
GraidanChristensen <graidanc@gmail.com>
2021-03-03 13:44:42 -0700

    favicon

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,9 +6,9 @@ class Contact extends Component{
     return (
         <div className="Contact" id="Contact">
             <div className="contactContainer">
-                <h3>Address</h3>
+                <h3>165 North 1330 West #A4 Orem, Utah 84057</h3>
                 <h3>1-801-874-0234<br/>wasatchmotoring@gmail.com</h3>
-                <h3>Hours:<br/>M-F: 9:00a.m. - 5:00p.m.</h3>
+                <h3>Hours:<br/>M-F: 9:00a.m. - 6:00p.m.</h3>
             </div>
         </div>
     )
