e0065ecf5b8c5f8e0ae02ab4767ad597d8d2f3d2
GraidanChristensen <graidanc@gmail.com>
2021-03-09 00:05:46 -0700

    email change

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component{
         <div className="Contact" id="Contact">
             <div className="contactContainer">
                 <h3>165 North 1330 West #A4 Orem, Utah 84057</h3>
-                <h3>1-801-874-0234<br/>wasatchmotoring@gmail.com</h3>
+                <h3>1-801-874-0234<br/>info@wasatchmotoring.com</h3>
                 <h3>Hours:<br/>M-F: 9:00a.m. - 6:00p.m.</h3>
             </div>
         </div>
