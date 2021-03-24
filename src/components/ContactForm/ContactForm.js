4c87d0d7c857a54393e8f39213807c96c5bf431e
GraidanChristensen <graidanc@gmail.com>
2021-03-24 17:44:51 -0600

    message sent

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,12 +2,21 @@ import './ContactForm.css';
 import emailjs from 'emailjs-com';
 import {Component} from 'react';
 class ContactForm extends Component{
+    constructor(){
+        super();
+        this.state={
+            toggleSent: false
+        }
+    }
 
     sendEmail = (e) => {
         e.preventDefault();
     
         emailjs.sendForm('gmail', 'template_xzdc195', e.target, 'user_vyovmBpG6SF8xDRiqVuIo')
           .then((result) => {
+              this.setState({
+                  toggleSent: true
+              })
               console.log(result.text);
           }, (error) => {
               console.log(error.text);
@@ -29,6 +38,7 @@ class ContactForm extends Component{
                     <input type="text" className="formInput" placeholder="Email" name="email"/>
                     <h3 className="formHeading">Message</h3>
                     <textarea className="formInput" id="" cols="30" rows="8" placeholder="Your message" name ="message"></textarea>
+                    <h4 className={this.state.toggleSent ? "sentMessage" : "hideMessage"}>Message Sent. We will reach out as soon as possible. Thank you</h4>
                     <input type="submit" className="submitButton" value="Send Message"></input>
                 </form>
             </div>
